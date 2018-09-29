f0502d3e6f35094dbd00cd252f8abcb1edd24775
Divya <dipsbhati6@gmail.com>
2018-09-29 18:26:28 -0500

    Added website files

---
+++ gas-data/src/navbar.js
@@ -0,0 +1,7 @@
+document.getElementById("navbar").innerHTML = 
+'<div class ="navbar">'+
+	'<a href="about.html"> <div> About </div> </a>'+
+	'<a href="station.html"> <div> Stations </div> </a>'+
+	'<a href="department.html"> <div> Departments </div> </a>'+
+	'<a href="driver.html"> <div> Drivers </div> </a>'+
+'</div>';
