a20de607b16af80b7008ac7da76c19af75bf4d20
ilievartic <32111470+ilievartic@users.noreply.github.com>
2018-09-29 21:54:32 -0500

    Add files via upload

--- gas-data/src/components/Map.js
+++ gas-data/src/components/Map.js
@@ -8,15 +8,119 @@ class Map extends Component {
         defaultZoom = { 13 }
       >
       <Marker
-      position={{ lat: 40.1106, lng: -88.2073 }}
+      position={{ lat: 40.1153841, lng: -88.18895309999999 }}
       icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
-      title= { 'name' }
+      title={ 'Urbana Farmtown' }
     />
       <Marker
-      position={{ lat: 40.11, lng: -88.2073 }}
+      position={{ lat: 40.13026119999999, lng: -88.19700849999998 }}
       icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
-      title= { 'name' }
+      title={ 'Freedom Oil Co.' }
     />
+      <Marker
+      position={{ lat: 40.1157165, lng: -88.18228140000002 }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'Caseys Gen. Store 323' }
+    />
+      <Marker
+      position={{ lat: 40.13338090000001, lng: -88.25878410000001 }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'Mach. 1 Food Shop 19' }
+    />
+      <Marker
+      position={{ lat: 40.07382399999999 , lng: -88.24916100000002  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'Thorntons #0385 Q' }
+    />
+      <Marker
+      position={{ lat: 40.116098, lng: -88.2773459 }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'Thorntons #0386' }
+    />
+      <Marker
+      position={{ lat: 40.1110275, lng: -88.21876729999997 }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'Circle K #1434' }
+    />
+      <Marker
+      position={{ lat:40.05885529999999 , lng:-88.25148000000002  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'Circle K #1400' }
+    />
+      <Marker
+      position={{ lat: 40.3076943, lng: -88.1743156 }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'BP 8608770' }
+    />
+      <Marker
+      position={{ lat:40.2390213 , lng:-88.19000499999999  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'Caseys Gen. Store 149' }
+    />
+      <Marker
+      position={{ lat: 40.0860406, lng:-88.1873137  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'Meijer 247' }
+    />
+      <Marker
+      position={{ lat: 40.1162058-88.21325389999998, lng:  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'Circle K #1413' }
+    />
+      <Marker
+      position={{ lat:40.1125714 , lng: -88.27235840000003 }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'Circle K #1399' }
+    />
+      <Marker
+      position={{ lat:40.0950795 , lng:-88.19128490000003  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'BP 8617102' }
+    />
+      <Marker
+      position={{ lat:40.112804 , lng:-88.20422889999998  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'Schnucks Urbana Expr.' }
+    />
+      <Marker
+      position={{ lat:40.1086496 , lng:-88.20463219999999  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'SZBD Inc.' }
+    />
+      <Marker
+      position={{ lat:40.1160711 , lng:-88.22940549999998  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ 'Mac's Convenience Sto' }
+    />
+      <Marker
+      position={{ lat: , lng:  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ '' }
+    />
+      <Marker
+      position={{ lat: , lng:  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ '' }
+    />
+      <Marker
+      position={{ lat: , lng:  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ '' }
+    />
+      <Marker
+      position={{ lat: , lng:  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ '' }
+    />
+      <Marker
+      position={{ lat: , lng:  }}
+      icon={{ url: 'https://openclipart.org/image/2400px/svg_to_png/103027/1293960051.png', scaledSize: { width: 32, height: 32 }}}
+      title={ '' }
+    />
+
+
+
+
       </GoogleMap>
    ));
    return(
